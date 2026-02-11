bbdb7684c0557cd1a32960351330e4eeaa6a6037
Diogo Martins <165835485+MDA2AV@users.noreply.github.com>
2026-02-11 23:04:13 +0000

    Update Flask server routing and change default port

--- src/Servers/FlaskServer/app.py
+++ src/Servers/FlaskServer/app.py
@@ -1,13 +1,16 @@
 import sys
 from flask import Flask
+from werkzeug.routing import Rule
 
 app = Flask(__name__)
 
-@app.route("/", defaults={"path": ""})
-@app.route("/<path:path>")
+app.url_map.add(Rule('/', defaults={"path": ""}, endpoint='catch_all'))
+app.url_map.add(Rule('/<path:path>', endpoint='catch_all'))
+
+@app.endpoint('catch_all')
 def catch_all(path):
     return "OK"
 
 if __name__ == "__main__":
-    port = int(sys.argv[1]) if len(sys.argv) > 1 else 9002
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 8080
     app.run(host="0.0.0.0", port=port)
